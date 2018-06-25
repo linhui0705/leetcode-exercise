6f59d9ee7c3d976f9a222cbb0c33ebf6779b3c85
linhui0705 <linhui0705@gmail.com>
2018-06-25 14:09:38 +0800

    P1_TwoSum

---
+++ src/main/java/P1_TwoSum/Solution2.java
@@ -0,0 +1,33 @@
+package P1_TwoSum;
+
+import org.junit.Test;
+
+/**
+ * Title: Solution2
+ * Description:
+ * 解法2：暴力，换汤不换药，i!=j放到for循环，稍微快一丢丢
+ * Runtime: 36ms
+ *
+ * @author Lin Hui
+ * Created on 2018/6/25 10:34
+ */
+public class Solution2 {
+    @Test
+    public void testCase() {
+        int[] nums = {3,2,4};
+        for (int i : twoSum(nums, 6)) {
+            System.out.println(i);
+        }
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length && i != j; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{0, 0};
+    }
+}
