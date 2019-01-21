160da4efbac016fe94eddd8e5aba24816e15d6bf
Lin Hui <linhui0705@gmail.com>
2019-01-21 20:41:02 +0800

    P242

---
+++ src/main/java/P242_ValidAnagram/Solution2.java
@@ -0,0 +1,36 @@
+package P242_ValidAnagram;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Title: Solution2
+ * Description:
+ * Discuss看到的一个Good Solution
+ *
+ * @author Lin Hui
+ * Created on 2019-1-21 20:36
+ */
+public class Solution2 {
+    @Test
+    public void testCase() {
+        Assert.assertTrue(isAnagram("anagram", "nagaram"));
+        Assert.assertFalse(isAnagram("rat", "car"));
+    }
+
+    public boolean isAnagram(String s, String t) {
+        int[] alphabet = new int[26];
+        for (char c : s.toCharArray()) {
+            alphabet[c - 'a']++;
+        }
+        for (char c : t.toCharArray()) {
+            alphabet[c - 'a']--;
+        }
+        for (int i : alphabet) {
+            if (i != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
