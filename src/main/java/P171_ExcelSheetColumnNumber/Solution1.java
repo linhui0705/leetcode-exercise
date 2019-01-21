cb4ca443ed1147d2ebfad3d6ac4b3e7ad97d12c6
linhui0705 <linhui0705@gmail.com>
2019-01-21 17:41:06 +0800

    P171

---
+++ src/main/java/P171_ExcelSheetColumnNumber/Solution1.java
@@ -0,0 +1,32 @@
+package P171_ExcelSheetColumnNumber;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Title: Solution1
+ * Description:
+ *
+ * @author Lin Hui
+ * Created on 2019/1/21 17:33
+ */
+public class Solution1 {
+    @Test
+    public void testCase() {
+        Assert.assertEquals(1, titleToNumber("A"));
+        Assert.assertEquals(28, titleToNumber("AB"));
+        Assert.assertEquals(701, titleToNumber("ZY"));
+    }
+
+    public int titleToNumber(String s) {
+        int result = 0;
+        char[] arrays = s.toCharArray();
+
+        for (int i = 0; i < s.length(); i++) {
+            int k = arrays[i] - 'A' + 1;
+            result += k * Math.pow(26, s.length() - 1 - i);
+        }
+
+        return result;
+    }
+}
