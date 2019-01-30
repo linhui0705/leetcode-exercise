e03844711c5e39033199b7fd2068691c0a0ed8d5
Lin Hui <linhui0705@gmail.com>
2019-01-30 22:21:40 +0800

    P263

---
+++ src/main/java/P263_UglyNumber/Solution1.java
@@ -0,0 +1,38 @@
+package P263_UglyNumber;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Title: Solution1
+ * Description:
+ * 判断丑数
+ * 需要注意的地方是，要判断0不是丑数
+ *
+ * @author Lin Hui
+ * Created on 2019-1-30 22:14
+ */
+public class Solution1 {
+    @Test
+    public void testCase() {
+        Assert.assertTrue(isUgly(6));
+        Assert.assertTrue(isUgly(8));
+        Assert.assertFalse(isUgly(14));
+    }
+
+    public boolean isUgly(int num) {
+        if (num == 0) {
+            return false;
+        }
+        while (num % 2 == 0) {
+            num /= 2;
+        }
+        while (num % 3 == 0) {
+            num /= 3;
+        }
+        while (num % 5 == 0) {
+            num /= 5;
+        }
+        return num == 1;
+    }
+}
