67e0e650c818f40dd23c9743f8d149a6485d5b06
linhui0705 <linhui0705@gmail.com>
2019-01-31 15:47:19 +0800

    P970

---
+++ src/main/java/P970_PowerfulIntegers/Solution1.java
@@ -0,0 +1,47 @@
+package P970_PowerfulIntegers;
+
+import org.junit.Test;
+
+import java.util.*;
+
+/**
+ * Title: Solution1
+ * Description:
+ * 初步是考虑暴力，两个for循环直接扫，但是超时了。原因是：如果x或y为1，会导致1的n次方永远是1，跳不出循环。
+ * <p>
+ * If x^i>bound the sum x^i+y^j can't be less than or equal to the bound. Similarly for y^j.
+ * Thus, we only have to check for 0<=i,j<=logx(bound)<18.
+ * We can use a HashSet to store all the different values.
+ * 至于Solution里的小于18，不太明白是怎么推出来的……
+ *
+ * @author Lin Hui
+ * Created on 2019/1/31 15:11
+ */
+public class Solution1 {
+    @Test
+    public void testCase() {
+        powerfulIntegers(2, 3, 10);
+        powerfulIntegers(3, 5, 15);
+        powerfulIntegers(1, 2, 15);
+        powerfulIntegers(1, 2, 100);
+    }
+
+    public List<Integer> powerfulIntegers(int x, int y, int bound) {
+        Set<Integer> result = new HashSet<>();
+        for (int i = 0; Math.pow(x, i) <= bound; i++) {
+            for (int j = 0; Math.pow(y, j) <= bound; j++) {
+                int k = (int) (Math.pow(x, i) + Math.pow(y, j));
+                if (k <= bound) {
+                    result.add(k);
+                }
+                if (y == 1) {
+                    break;
+                }
+            }
+            if (x == 1) {
+                break;
+            }
+        }
+        return new ArrayList<>(result);
+    }
+}
