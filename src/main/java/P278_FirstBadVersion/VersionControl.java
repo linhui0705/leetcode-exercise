0de18efd206647d02bdf7fb13e8a5001278c0d50
Lin Hui <linhui0705@gmail.com>
2019-02-10 11:05:29 +0800

    P278

---
+++ src/main/java/P278_FirstBadVersion/VersionControl.java
@@ -0,0 +1,15 @@
+package P278_FirstBadVersion;
+
+/**
+ * Title: VersionControl
+ * Description:
+ *
+ * @author Lin Hui
+ * Created on 2019-2-10 10:20
+ */
+public class VersionControl {
+    public boolean isBadVersion(int version) {
+        //start from 16
+        return version > 0;
+    }
+}
