385d1ec3571c3baf415d3187645f46d59440b65e
Lin Hui <linhui0705@gmail.com>
2019-02-12 11:50:10 +0800

    P021

---
+++ src/main/java/P21_MergeTwoSortedLists/Solution1.java
@@ -0,0 +1,65 @@
+package P21_MergeTwoSortedLists;
+
+import org.junit.Test;
+
+/**
+ * Title: Solution1
+ * Description:
+ * 将两个有序的链表合并为一个有序的链表
+ *
+ * @author Lin Hui
+ * Created on 2019-2-12 11:32
+ */
+public class Solution1 {
+    @Test
+    public void testCase() {
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(3);
+        l1.next = new ListNode(5);
+
+        ListNode l2 = new ListNode(2);
+        l2.next = new ListNode(4);
+        l2.next = new ListNode(6);
+
+        ListNode l3 = mergeTwoLists(l1, l2);
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+
+        ListNode l3;
+        ListNode p;
+        if (l1.val < l2.val) {
+            l3 = new ListNode(l1.val);
+            l1 = l1.next;
+        } else {
+            l3 = new ListNode(l2.val);
+            l2 = l2.next;
+        }
+        p = l3;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                p.next = l1;
+                l1 = l1.next;
+            } else {
+                p.next = l2;
+                l2 = l2.next;
+            }
+            p = p.next;
+        }
+
+        if (l1 != null) {
+            l3.next = l1;
+        } else {
+            l3.next = l2;
+        }
+        return l3;
+    }
+}
