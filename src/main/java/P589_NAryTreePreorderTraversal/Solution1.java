b7ccb3c00e6a062c01cad152bf791d305489ad5b
Lin Hui <linhui0705@gmail.com>
2019-02-19 21:15:56 +0800

    P589

---
+++ src/main/java/P589_NAryTreePreorderTraversal/Solution1.java
@@ -0,0 +1,39 @@
+package P589_NAryTreePreorderTraversal;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Title: Solution1
+ * Description:
+ * N叉树的先序遍历
+ * 按照“根-左-右”的顺序进行遍历。
+ *
+ * @author Lin Hui
+ * Created on 2019-2-19 21:11
+ */
+public class Solution1 {
+    List<Integer> ans = new ArrayList<>();
+
+    @Test
+    public void testCase() {
+
+    }
+
+    public List<Integer> preorder(Node root) {
+        traversal(root);
+        return ans;
+    }
+
+    public void traversal(Node root) {
+        if (root == null) {
+            return;
+        }
+        ans.add(root.val);
+        for (Node node : root.children) {
+            traversal(node);
+        }
+    }
+}
