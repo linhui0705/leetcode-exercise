2940aaf11f0034bc3b1c1a7e2a9b7a7c7c749ee0
linhui0705 <linhui0705@gmail.com>
2019-02-21 16:13:17 +0800

    P257

---
+++ src/main/java/P257_BinaryTreePaths/TreeNode.java
@@ -0,0 +1,18 @@
+package P257_BinaryTreePaths;
+
+/**
+ * Title: TreeNode
+ * Description:
+ *
+ * @author Lin Hui
+ * Created on 2019-2-19 22:11
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
