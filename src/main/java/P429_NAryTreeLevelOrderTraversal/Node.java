9cdc93f99486f9854e56618477843f5455a62112
linhui0705 <linhui0705@gmail.com>
2019-02-22 17:29:49 +0800

    P429

---
+++ src/main/java/P429_NAryTreeLevelOrderTraversal/Node.java
@@ -0,0 +1,23 @@
+package P429_NAryTreeLevelOrderTraversal;
+
+import java.util.List;
+
+/**
+ * Title: Node
+ * Description:
+ *
+ * @author Lin Hui
+ * Created on 2019-2-19 21:12
+ */
+public class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
---
+++ src/main/java/P429_NAryTreeLevelOrderTraversal/Solution1.java
@@ -0,0 +1,45 @@
+package P429_NAryTreeLevelOrderTraversal;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Title: Solution1
+ * Description:
+ * 遍历N叉树，按照层级顺序，输出层级结点List
+ *
+ * @author Lin Hui
+ * Created on 2019/2/22 17:24
+ */
+public class Solution1 {
+    List<List<Integer>> ans = new ArrayList<>();
+
+    @Test
+    public void testCase() {
+
+    }
+
+    public List<List<Integer>> levelOrder(Node root) {
+        traversal(root, 0);
+        return ans;
+    }
+
+    public void traversal(Node root, int depth) {
+        if (null == root) {
+            return;
+        }
+        List<Integer> list = null;
+        if (ans.size() <= depth) {
+            list = new ArrayList<>();
+            ans.add(list);
+        } else {
+            list = ans.get(depth);
+        }
+        list.add(root.val);
+        for (Node node : root.children) {
+            traversal(node, depth + 1);
+        }
+    }
+}
