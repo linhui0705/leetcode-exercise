e1e639815821a05cd10d1f3ca6c3c17af2dcd1eb
linhui0705 <linhui0705@gmail.com>
2020-02-21 22:59:19 +0800

    P0002

--- src/main/java/P0002_AddTwoNumbers/Solution1.java
+++ src/main/java/P0002_AddTwoNumbers/Solution1.java
@@ -4,6 +4,7 @@ package P0002_AddTwoNumbers;
  * Title: Solution1
  * Description:
  * 给定两条链表l1和l2，链表顺序表示该数的逆序。如l1: (2->4->3)，表示342，l2: (5->6->4)，表示465，两数和为342+465=807，返回的链表为7->0->8
+ * 将l2上的数加到l1链表上，最后的结果返回l1链表即可。需要小心的地方就是要处理好两条链表的末尾的情况。
  *
  * @author Lin Hui
  * Created on 2020/2/21 22:33
@@ -20,6 +21,7 @@ public class Solution1 {
             return l1;
         }
         ListNode p = l1;
+        //pb为p的上一个结点
         ListNode pb = null;
         ListNode q = l2;
         while (null != p && null != q) {
