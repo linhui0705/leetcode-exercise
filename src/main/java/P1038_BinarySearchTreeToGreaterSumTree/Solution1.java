2fe5ecba85a3923efb0bd682acda11c1186a9b0b
linhui0705 <linhui0705@gmail.com>
2022-02-20 00:00:29 +0800

    P1038

---
+++ src/main/java/P1038_BinarySearchTreeToGreaterSumTree/Solution1.java
@@ -0,0 +1,30 @@
+package P1038_BinarySearchTreeToGreaterSumTree;
+
+/**
+ * Title: Solution1
+ * Description:
+ * 给定一个二叉搜索树，将这个树转换为累积树，使每个节点 node 的新值等于原树中大于或等于 node.val 的值之和。
+ * 题目与538相同。
+ *
+ * @author Lin Hui
+ * Created on 2022/2/19 23:59:09
+ */
+public class Solution1 {
+    int sum;
+
+    public TreeNode bstToGst(TreeNode root) {
+        sum = 0;
+        traverse(root);
+        return root;
+    }
+
+    private void traverse(TreeNode node) {
+        if (null == node) {
+            return;
+        }
+        traverse(node.right);
+        node.val += sum;
+        sum = node.val;
+        traverse(node.left);
+    }
+}
---
+++ src/main/java/P1038_BinarySearchTreeToGreaterSumTree/TreeNode.java
@@ -0,0 +1,18 @@
+package P1038_BinarySearchTreeToGreaterSumTree;
+
+/**
+ * Title: TreeNode
+ * Description:
+ *
+ * @author Lin Hui
+ * Created on 2020/2/11 12:58
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
